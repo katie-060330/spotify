678923d272d714de3d462cc5a89f32c8a7b1b335
katie-060330 <kate.lonsway@outlook.com>
2024-12-14 16:20:29 -0500

    inintal commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// music_data_2017 = []
+// music_data_2018 = []
+// music_data_2019 = []
+// music_data_2020 = []
+// music_data_2021 = []
+// music_data_2022 = []
+// music_data_2023 = []
+// music_data_2024 = []
+
+// fetch('./data/Streaming_History_Audio_2017-2019_0.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     data.forEach( e =>{
+//         switch(e.ts.slice(0,4)){
+//             case "2017":
+//                 music_data_2017.push(e)
+//                 break; 
+//             case "2018":
+//                 music_data_2018.push(e)
+//                 break; 
+//             case "2019":
+//                 music_data_2019.push(e)
+//                 break; 
+//             case "2020":
+//                 music_data_2020.push(e)
+//                 break; 
+//             case "2021":
+//                 music_data_2021.push(e)
+//                 break; 
+//             case "2022":
+//                 music_data_2022.push(e)
+//                 break; 
+//             case "2023":
+//                 music_data_2023.push(e); 
+//                 break; 
+//             case "2024":
+//                 music_data_2024.push(e); 
+//                 break; 
+//             default:
+//                 break; 
+                
+
+//         }
+//     })
+//   })
+
+//   .catch(error => console.error('Error:', error));
+
+//   console.log(music_data_2019);
+
+
+
+  
