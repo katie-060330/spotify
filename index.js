b730f4411c90c5a676989de4d78617f0dfe2bdce
katie-060330 <kate.lonsway@outlook.com>
2024-12-14 17:00:08 -0500

    setting the years

--- index.js
+++ index.js
@@ -1,53 +1,48 @@
-// music_data_2017 = []
-// music_data_2018 = []
-// music_data_2019 = []
-// music_data_2020 = []
-// music_data_2021 = []
-// music_data_2022 = []
-// music_data_2023 = []
-// music_data_2024 = []
+one7 = document.getElementById("17");
+one8 = document.getElementById("18");
+one9 = document.getElementById("19");
+two0 = document.getElementById("20");
+two1 = document.getElementById("21");
+two2 = document.getElementById("22");
+two3 = document.getElementById("23");
+two4 = document.getElementById("24");
 
-// fetch('./data/Streaming_History_Audio_2017-2019_0.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     data.forEach( e =>{
-//         switch(e.ts.slice(0,4)){
-//             case "2017":
-//                 music_data_2017.push(e)
-//                 break; 
-//             case "2018":
-//                 music_data_2018.push(e)
-//                 break; 
-//             case "2019":
-//                 music_data_2019.push(e)
-//                 break; 
-//             case "2020":
-//                 music_data_2020.push(e)
-//                 break; 
-//             case "2021":
-//                 music_data_2021.push(e)
-//                 break; 
-//             case "2022":
-//                 music_data_2022.push(e)
-//                 break; 
-//             case "2023":
-//                 music_data_2023.push(e); 
-//                 break; 
-//             case "2024":
-//                 music_data_2024.push(e); 
-//                 break; 
-//             default:
-//                 break; 
-                
 
-//         }
-//     })
-//   })
-
-//   .catch(error => console.error('Error:', error));
-
-//   console.log(music_data_2019);
+one7.addEventListener("click", function () {
+  fetchSevenTeenData();
+  setPage(); 
+});
 
 
 
+let fetchSevenTeenData = () => {
+  music_data_2017 = [];
+  music_data_2018 = [];
+  music_data_2019 = [];
+  let counter; 
   
+
+  fetch("./data/Streaming_History_Audio_2017-2019_0.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((e) => {
+        counter += e.ts; 
+        switch (e.ts.slice(0, 4)) {
+          case "2017":
+            music_data_2017.push(e);
+            break;
+          case "2018":
+            music_data_2018.push(e);
+            break;
+          case "2019":
+            music_data_2019.push(e);
+            break;
+          default:
+            break;
+        }
+      });
+    })
+    minListen.textContent  = (counter * 0.00001667)
+
+    .catch((error) => console.error("Error:", error));
+};
---
+++ years.js
@@ -0,0 +1,4 @@
+let setPage = () =>{
+    document.querySelector(".setYear").innerHTML = 2017; 
+}
+setPage(); 
